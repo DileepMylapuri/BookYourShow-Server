0415553596f602b6917584346cbc18e88584eb60
DileepLee <mylapuridileep@gmail.com>
2026-05-25 11:29:15 +0530

    Restore deleted source files (package.json, server.js, etc)

---
+++ database.js
@@ -0,0 +1,20 @@
+// database.js
+const { MongoClient } = require('mongodb');
+
+let databaseConnection;
+
+module.exports = {
+  connectToDb: (cb) => {
+    MongoClient.connect(process.env.MONGO_URI || "mongodb://localhost:27017")
+      .then((client) => {
+        databaseConnection = client.db('UserData');
+        console.log('✅ MongoDB Connected to UserData...');
+        return cb();
+      })
+      .catch((error) => {
+        console.error('MongoDB connection error:', error);
+        return cb(error);
+      });
+  },
+  getDb: () => databaseConnection,
+};
