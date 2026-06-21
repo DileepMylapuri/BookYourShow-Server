3f28c368f28bc7873f3cb6d40dc87f377177f595
DileepLee <mylapuridileep@gmail.com>
2026-06-21 21:16:05 +0530

    Use Gmail SMTP port 465 SSL for nodemailer on Render

--- server.js
+++ server.js
@@ -226,9 +226,8 @@ app.post("/api/send-booking-email", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      requireTLS: true,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
